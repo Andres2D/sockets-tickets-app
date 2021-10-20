5a1e11957173ac04186f254d5d7a824111fb303e
Andres2D <andres2d1997.1206@gmail.com>
2021-10-19 20:32:43 -0500

    Add tickets controls system

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,52 @@
 
+// Refs HTML
+const lblDesktop = document.querySelector('h1');
+const lblTicket = document.querySelector('small');
+const btnNew = document.querySelector('button');
+const divAlert = document.querySelector('.alert');
+const lblPending = document.querySelector('#lblPending');
 
-console.log('Escritorio HTML');
+const searchParams = new URLSearchParams(window.location.search);
+
+if(!searchParams.has('desktop')) {
+    window.location = 'index.html';
+    throw new Error('The desktop is required');
+}
+
+const desktop = searchParams.get('desktop');
+divAlert.style.display = 'none';
+lblDesktop.innerText = desktop;
+
+const socket = io();
+
+socket.on('connect', () => {
+    btnNew.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnNew.disabled = true;
+});
+
+socket.on('pending-tickets', (pending) => {
+    console.log(pending);
+    lblPending.innerText = pending;
+});
+
+btnNew.addEventListener( 'click', () => {
+    
+    socket.emit('attend-ticket', {desktop}, ({ok, ticket, msg}) => {
+        if(!ok) {
+            lblTicket.innerText = 'Nadie.'; 
+            return divAlert.style.display = '';
+        }
+        lblTicket.innerText = `The ticket ${ticket.number}`;       
+    });
+
+    socket.emit('pending-tickets', () => {
+
+    })
+
+//     socket.emit( 'next-ticket', null, ( ticket ) => {
+//        lblNewTicket.innerText = ticket;
+//     });
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,15 +1,47 @@
+const TicketControl = require('../models/ticket-control');
+
+const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
-    console.log('Client connected', socket.id);
-    socket.on('disconnect', () => {
-        console.log('Client disonnected', socket.id);
+
+    socket.emit('last-ticket', ticketControl.last);
+    socket.emit('current-state', ticketControl.lastFour);
+
+    socket.emit('pending-tickets', ticketControl.tickets.length);
+
+    socket.on('next-ticket', (payload, callback) => {
+        const next = ticketControl.next();
+        socket.broadcast.emit('pending-tickets', ticketControl.tickets.length);
+        callback(next);
+
+        // TODO: notify the new ticket pending
     });
 
-    socket.on('send-message', (payload, callback) => {
-        const id = 123456789;
-        callback(id);
-        socket.broadcast.emit('send-message', payload);
-    });
+    socket.on('attend-ticket', ({desktop}, callback) => {
+        if(!desktop) {
+            return callback({
+                ok: false,
+                msg: 'The desktop is required'
+            });
+        }
+
+        const ticket = ticketControl.attend(desktop);
+        socket.broadcast.emit('current-state', ticketControl.lastFour);
+        socket.emit('pending-tickets', ticketControl.tickets.length);
+        socket.broadcast.emit('pending-tickets', ticketControl.tickets.length);
+
+        if(!ticket) {
+            callback({
+                ok: false,
+                mag: 'No more pending tickets'
+            });
+        }else {
+            callback({
+                ok: true,
+                ticket
+            });
+        }
+    })
 }
 
 module.exports = {
